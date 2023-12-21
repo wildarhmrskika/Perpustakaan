649f870b6fbc30cd8f9c8cb8393f277b38b18e28
Mansadhit301 <aadhit407@gmail.com>
2023-12-21 18:52:04 +0800

    tambah konsep OOP

--- index.py
+++ index.py
@@ -1,79 +1,101 @@
 from flask import Flask, render_template, request, url_for, redirect
-import sqlite3 
-from konsepOOP import *
+from konsepOOP import DatabaseManager, Buku, KelolaBuku, User
+import sqlite3
 
-app = Flask('__name__') 
+app = Flask('__name__')
 
-conn = sqlite3.connect('db_belajar_jalan.db',check_same_thread=False,isolation_level=None)
-#update ke-3
-@app.route('/', methods = ['GET', 'POST'])
+db_manager = DatabaseManager("db_belajar_jalan.db")
+semua_buku = KelolaBuku("db_belajar_jalan.db")
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-	cur = conn.cursor()
-	data = cur.execute('select * from terbaru').fetchall()
 
-	a = tagar().fetchA()
-	if request.method == 'GET':
-		print(a)
-		return render_template('index.html', data=data,	a=a)
-	elif request.method == 'POST':
-		search = cur.execute('select * from terbaru where LOWER(judul)=LOWER("{}")'.format(request.form.get('ok'))).fetchall()	
-		if(search):
-			return render_template('cari.html', hebat=search)
-		else:
-			return render_template('notfound.html')
+    tagar = db_manager.execute_query("SELECT * FROM tagar").fetchall()
 
-@app.route('/dash', methods = ['GET','POST'])
-def dash():
-	Aj = request.form.get('Hebat')
-	Ak = request.form.get('kategori')
-	Ad = request.form.get('deskripsi')
-
-	if request.method == 'GET':
-		return render_template('info.html')
-	else:
-		curs = conn.cursor()
-		fs = request.files['sampul']
-		fp = request.files['file']
-
-		cek = curs.execute(f"insert into terbaru values (null,'{Aj}','{Ak}','{Ad}','{fp.filename}','{fs.filename}')")
-		fp.save('static/pdf/'+fp.filename)
-		fs.save('static/img/'+fs.filename)
-
-		conn.commit()
-		# return Aj
-		#return Ad
-
-		if cek:
-			return redirect(url_for('index'))
-		else:
-			return 'gagal'
+    if request.method == 'GET':
+        data_buku = semua_buku.tampilkan_buku()
+        return render_template('index.html', data_buku=data_buku,	tagar=tagar)
+    elif request.method == 'POST':
+        search_value = request.form.get("search")
+        buku = semua_buku.cari_buku(search_value)
+        if (buku):
+            return render_template('cari.html', data_buku=buku)
+        else:
+            return render_template('notfound.html')
 
 
+@app.route('/tambah_buku', methods=['GET', 'POST'])
+def tambah_buku():
 
-@app.route('/admin', methods = ['GET', 'POST'])
+    if request.method == 'GET':
+        return render_template('tambah_buku.html')
+    elif request.method == 'POST':
+
+        judul = request.form.get('judul')
+        kategori = request.form.get('kategori')
+        deskripsi = request.form.get('deskripsi')
+        file = request.files['file']
+        sampul = request.files['sampul']
+
+        buku_baru = Buku("db_belajar_jalan.db", judul, kategori,
+                         deskripsi, file.filename, sampul.filename)
+        buku_baru.tambah_data()
+
+        file.save('static/pdf/'+file.filename)
+        sampul.save('static/img/'+sampul.filename)
+
+        if buku_baru:
+            return redirect(url_for('index'))
+        else:
+            return 'gagal'
+
+
+@app.route('/admin', methods=['GET', 'POST'])
 def login():
-	user = request.form.get('user')
-	pw = request.form.get('pass')
-	if request.method == 'GET':
-		return render_template('login.html')
-	elif request.method == 'POST':
-		l = Login(user,pw)
-		if l.Check() == '200':
-			return redirect(url_for('dash'))
-		else:
-			return render_template('login.html')
+    if request.method == 'GET':
+        return render_template('login.html')
+    elif request.method == 'POST':
+        username = request.form.get('user')
+        password = request.form.get('pass')
 
-@app.route('/baca/<int:keren>')
-def baca(keren):
-	cur = conn.cursor()
-	medata = cur.execute(f'select * from terbaru where id={keren}').fetchall()	
-	print(medata)
-	return render_template('read.html',medata=medata)
+        user = User("db_belajar_jalan.db", username, password)
+        if user.cek_user() == '200':
+            return redirect(url_for('tambah_buku'))
+        else:
+            return render_template('login.html')
 
-@app.route('/tags/<fantastik>')
-def abc(fantastik):
-	cur = conn.cursor()
-	panini = cur.execute(f"SELECT * FROM terbaru WHERE kategori='{fantastik}'").fetchall()
-	return render_template('tags.html',panini=panini)
+@app.route('/daftar', methods=['POST', 'GET'])
+def daftar():
+    username = request.form.get('username')
+    password = request.form.get('password')
+    if request.method == 'GET':
+        return render_template('daftar.html')
+    else:
+        user = User("db_belajar_jalan.db", username, password)
+        user.tambah_data()
+        if (user):
+            return 'berhasil terdaftar'
+        else:
+            return 'tidak berhasil'
+
+
+@app.route('/baca/<int:id_buku>')
+def baca(id_buku):
+    buku = semua_buku.baca_buku(id_buku)
+    return render_template('read.html', buku=buku)
+
+
+@app.route('/tags/<kategori>')
+def cari_buku_kategori(kategori):
+    buku = semua_buku.tampil_berdasarkan_kategori(kategori)
+    return render_template('tags.html', buku=buku)
+
+
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
+
+
+@app.teardown_appcontext
+def close_db_connection(exception=None):
+    db_manager.close_connection()
--- konsepOOP.py
+++ konsepOOP.py
@@ -1,31 +1,76 @@
-import sqlite3 
- 
-class FetchDb:
-	def __init__(self):
-		self.conn = sqlite3.connect('db_belajar_jalan.db',isolation_level=None)
+import sqlite3
 
-	def fetchA(self):
-		data = self.conn.execute('select * from terbaru').fetchall()
-		self.conn.close()
-		return data
-#fetch tagar
-class tagar(FetchDb):
-	def fetchA(self):
-		data = self.conn.execute('select * from tagar').fetchall()
-		self.conn.close()
-		return data
+class DatabaseManager:
+    def __init__(self, db_file):
+        self.conn = self.create_connection(db_file)
 
-class Login:
+    def create_connection(self, db_file):
+        conn = sqlite3.connect(
+            db_file, check_same_thread=False, isolation_level=None)
+        return conn
 
-	def __init__(self,user,pword):
-		self.user = user
-		self.__pasw = pword
+    def close_connection(self):
+        if self.conn:
+            self.conn.close()
 
-	def Check(self):
-		if self.user == 'admin':
-			if self.__pasw == '123':
-				print('OK')
-				return '200'
-		else :
-			print('ok')
-			return '404'
+    def execute_query(self, query):
+        cursor = self.conn.cursor()
+        cursor.execute(query)
+        self.conn.commit()
+        return cursor
+
+
+class Buku(DatabaseManager):
+    def __init__(self, db_file, judul, kategori, deskripsi, file, sampul):
+        super().__init__(db_file)
+        self.judul = judul
+        self.kategori = kategori
+        self.deskripsi = deskripsi
+        self.file = file
+        self.sampul = sampul
+
+    def tambah_data(self):
+        query = f"INSERT INTO terbaru VALUES (null, '{self.judul}', '{self.kategori}', '{self.deskripsi}', '{self.file}', '{self.sampul}')"
+        self.execute_query(query)
+
+
+class KelolaBuku(DatabaseManager):
+    def tampilkan_buku(self):
+        query = "SELECT * FROM terbaru"
+        return self.execute_query(query).fetchall()
+
+    def tampil_berdasarkan_kategori(self, kategori):
+        query = f"SELECT * FROM terbaru WHERE kategori = '{kategori}'"
+        return self.execute_query(query).fetchall()
+
+    def cari_buku(self, search_value):
+        query = f"SELECT * FROM terbaru WHERE LOWER(judul)=LOWER('{search_value}')"
+        return self.execute_query(query).fetchall()
+
+    def baca_buku(self, id_buku):
+        query = f'select * from terbaru where id={id_buku}'
+        return self.execute_query(query).fetchall()
+
+
+class User(DatabaseManager):
+    def __init__(self, db_file, username, password):
+        super().__init__(db_file)
+        self.username = username
+        self.__password = password
+
+    def tambah_data(self):
+        query = f"INSERT INTO admin VALUES ('{self.username}', '{self.__password}')"
+        self.execute_query(query) 
+
+    def ambil_data(self):
+        query = f"select * from admin where username='{self.username}'"
+        data = self.execute_query(query).fetchall()
+        return data
+
+    def cek_user(self):
+        data = self.ambil_data()
+        if self.username == data[0][0]:
+            if self.__password == data[0][1]:
+                return "200"
+        else:
+            return "404"
